d0175612761e5d88c73d9fab2e01b4972908cb12
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-09-16 21:53:28 -0500

    Chore: orgainized css property lists

--- Projects/ChallengeSet02/Challenge01/script.js
+++ Projects/ChallengeSet02/Challenge01/script.js
@@ -1,15 +1,16 @@
 const toggle = document.querySelector(".toggle");
 const menu = document.querySelector(".menu");
-const heading = document.querySelector(".heading")
+const heading = document.querySelector(".heading");
+
 function toggleMenu() {
 	if (menu.classList.contains("active")) {
 		menu.classList.remove("active");
-        heading.classList.remove("active");
+		heading.classList.remove("active");
 
 		toggle.innerHTML = "<i class='fas fa-bars'></i>";
 	} else {
 		menu.classList.add("active");
-        heading.classList.add("active");
+		heading.classList.add("active");
 		toggle.innerHTML = "<span>X<i class=’fas fa-times’></i></span>";
 	}
 }
